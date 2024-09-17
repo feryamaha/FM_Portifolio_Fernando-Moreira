bb842994b74cbc4d2bb5fe18c8b214dd9fdc4fc9
Fernando Moreira <feryamaha@hotmail.com>
2024-09-17 13:11:34 -0300

    Segundo commit
    Ajuste e correções!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
 
+import Avatar from "../src/img/logo_FM.png";
+
 import "./styles/components/app.sass";
 
 function App() {
   return (
     <div id="portfolio">
-      <h1>Fernando Moreira</h1>
+      <h1>
+        <img id="img-logo" src={Avatar} alt="Fernando Moreira" />
+      </h1>
       <Sidebar />
       <MainContent />
     </div>
