0881ed0c25c7efe6eb07fe6f46c02ff41f3f6926
Fernando Moreira <90003870+feryamaha@users.noreply.github.com>
2024-09-19 18:08:07 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
-import Avatar from "../src/img/logo_FM.png";
+import Avatar from "./src/img/logo_FM.png";
 import "./styles/components/app.sass";
 
 function App() {
