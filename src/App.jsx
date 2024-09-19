2aa72a42df4f019d7e0b666485a40fa576829098
Fernando Moreira <feryamaha@hotmail.com>
2024-09-18 22:26:40 -0300

    Correção deploy github
    Correção deploy github pages, configurado o Router-dom do projeto no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainContent from "./components/MainContent";
-
 import Sidebar from "./components/Sidebar";
-
 import Avatar from "../src/img/logo_FM.png";
-
 import "./styles/components/app.sass";
 
 function App() {
   return (
-    <div id="portfolio">
-      <h1>
-        <img id="img-logo" src={Avatar} alt="Fernando Moreira" />
-      </h1>
-      <Sidebar />
-      <MainContent />
-    </div>
+    <Router basename="/FM_Portifolio_Fernando-Moreira">
+      <div id="portfolio">
+        <h1>
+          <img id="img-logo" src={Avatar} alt="Fernando Moreira" />
+        </h1>
+        <Sidebar />
+        <MainContent />
+      </div>
+    </Router>
   );
 }
 
