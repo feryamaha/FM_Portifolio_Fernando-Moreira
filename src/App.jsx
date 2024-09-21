305d6cc703634d7cbeefe977b26de19b7e4e81e0
Fernando Moreira <feryamaha@hotmail.com>
2024-09-21 14:50:07 -0300

    Ajuste no app e vite.config
    Ajuste de rota no App.jsx e no vite.config.js estavam com a rota do projeto errado.!

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
-import Avatar from "./src/img/logo_FM.png";
+import Avatar from "./img/logo_FM.png";
 import "./styles/components/app.sass";
 
 function App() {
   return (
     <Router basename="/FM_Portifolio_Fernando-Moreira">
-      <div id="portfolio">
-        <h1>
-          <img id="img-logo" src={Avatar} alt="Fernando Moreira" />
-        </h1>
-        <Sidebar />
-        <MainContent />
-      </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route
+          path="/home"
+          element={
+            <div id="portfolio">
+              <h1>
+                <img id="img-logo" src={Avatar} alt="Fernando Moreira" />
+              </h1>
+              <Sidebar />
+              <MainContent />
+            </div>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
